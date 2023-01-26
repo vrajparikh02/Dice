0cc6b6512858b334c8d8aa13a11ec5fc1c256fab
vrajparikh02 <85411665+vrajparikh02@users.noreply.github.com>
2023-01-26 17:51:45 -0500

    Add files via upload

---
+++ Dice.py
@@ -0,0 +1,18 @@
+import random
+roll=(input("Y to roll N to exit "))
+for i in range (roll=="y"):
+    no=random.randint(1,6)
+y=yes
+yes=(roll=="y")
+n=nop
+nop=(roll=="n")
+if roll<6:
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+else:
+  yes  
+
